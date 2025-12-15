722823f76519dff200ad47af65b9b2f0c07cb5b5
Nathaniel Idyege <idyegenathaniel@gmail.com>
2025-12-15 17:43:43 +0100

    fix

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,25 +1,25 @@
 import { Collections } from "@/components/Collections";
 import { ProductsList } from "@/components/ProductsList";
 import HeroSlider from "@/components/Slider";
+import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import { Suspense } from "react";
-import Loading from "../loading";
 
 export default function Home() {
   return (
     <Box>
       <Box component="section">
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={<Typography>Loading...</Typography>}>
           <HeroSlider />
         </Suspense>
       </Box>
       <Box sx={{ my: { xs: 2, md: 5 }, padding: "0 24px" }}>
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={<Typography>Loading...</Typography>}>
           <ProductsList />
         </Suspense>
       </Box>
       <Box sx={{ my: { xs: 2, md: 5 }, padding: "0 24px" }}>
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={<Typography>Loading...</Typography>}>
           <Collections />
         </Suspense>
       </Box>
