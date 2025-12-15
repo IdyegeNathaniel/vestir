433a6c0c3dae800c3a9a39b2310b4f990c67621e
Nathaniel Idyege <idyegenathaniel@gmail.com>
2025-12-15 17:38:27 +0100

    Bug fix

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -2,23 +2,27 @@ import { Collections } from "@/components/Collections";
 import { ProductsList } from "@/components/ProductsList";
 import HeroSlider from "@/components/Slider";
 import Box from "@mui/material/Box";
-
-
-const backGround = {
-}
+import { Suspense } from "react";
+import Loading from "../loading";
 
 export default function Home() {
-    return (
-        <Box>
-            <Box component="section" style={backGround}>
-                <HeroSlider />
-            </Box>
-            <Box sx={{ my: { xs: 2, md: 5 }, padding: "0 24px" }}>
-                <ProductsList />
-            </Box>
-            <Box sx={{ my: { xs: 2, md: 5 }, padding: "0 24px" }}>
-                <Collections />
-            </Box>
-        </Box>
-    )
+  return (
+    <Box>
+      <Box component="section">
+        <Suspense fallback={<Loading />}>
+          <HeroSlider />
+        </Suspense>
+      </Box>
+      <Box sx={{ my: { xs: 2, md: 5 }, padding: "0 24px" }}>
+        <Suspense fallback={<Loading />}>
+          <ProductsList />
+        </Suspense>
+      </Box>
+      <Box sx={{ my: { xs: 2, md: 5 }, padding: "0 24px" }}>
+        <Suspense fallback={<Loading />}>
+          <Collections />
+        </Suspense>
+      </Box>
+    </Box>
+  );
 }
